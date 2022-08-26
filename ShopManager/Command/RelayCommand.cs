934d6b387cbd26fcd35bf9e91e020057bdcdcb56
Дмитрий <deathlukish@mail.ru>
2022-08-26 08:38:41 +0300

    Добавьте файлы проекта.

---
+++ ShopManager/Command/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManager.Command
+{
+    internal class RelayCommand : BaseCommand
+
+    {
+        private readonly Action<object> _Action;
+        private readonly Func<object, bool> _CanExecute;
+        public RelayCommand(Action<object> Action, Func<object, bool> CanExecute)
+        {
+            _Action = Action ?? throw new ArgumentNullException(nameof(Action));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Action(parameter);
+    }
+
+
+}
+
---
+++ ShopManager/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ShopManager.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ShopManager.ViewModel
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        public ICommand command { get; }
+        private bool CanClose(object p) => true;
+        private void OnClose(object p) => MessageBox.Show("re");
+
+
+        public MainWindowViewModel()
+        {
+
+            command = new RelayCommand(OnClose, CanClose);
+        
+        }
+
+    }
+}
