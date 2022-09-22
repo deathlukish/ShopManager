1d8af585c68f33f117e6e829fde66f022548570f
Дмитрий <deathlukish@mail.ru>
2022-09-22 16:14:36 +0300

    tests co

--- ShopManager/ProductBase.cs
+++ ShopManager/ProductBase.cs
@@ -12,63 +12,40 @@ namespace ShopManager
         public event Action<string>? _update;
         private DataTable dt;
         private SqlDataAdapter da;
-        private SqlCommandBuilder commandBuilder;
+       // private SqlCommandBuilder commandBuilder;
         private SqlConnection con;
         public ProductBase()
-        {
-           
+        {           
             da = new SqlDataAdapter();
             dt = new DataTable();
-            commandBuilder = new SqlCommandBuilder(da);
+          //  commandBuilder = new SqlCommandBuilder(da);
             con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectProducts"].ConnectionString);
-
+            da.InsertCommand = new SqlCommand("SELECT * FROM Cart",con);
+            da.UpdateCommand = new SqlCommand("SELECT * FROM Cart", con);            
         }
-
-
- 
-
         /// <summary>
         /// Получить все продукты клиента из базы
         /// </summary>
         /// <returns></returns>
         public  DataTable GetProducts(string eMail)
         {
-
-//            @"SELECT 
-//Workers.id as 'ID',
-//Workers.workerName as 'Имя сотрудника',
-//Bosses.workerName  as 'Имя начальника',
-//Bosses.departmentName  as 'Имя отдела',
-//[Description].[value] as 'Замечание'
-//FROM  Workers, Bosses, [Description]
-//WHERE Workers.idBoss = Bosses.id and Workers.idDescription = [Description].id
-//Order By Workers.Id";
-
-
-
             dt.Clear();
             string SelectCommand = $"SELECT " +
                 $"Products.nameProd as 'Наименование'," +
                 $"Products.Price as 'Цена'" +
                 $"FROM Cart, Products WHERE Cart.eMail = '{eMail}' and Products.idProd = Cart.idProd ";
-            da.SelectCommand = new SqlCommand();
             GetProd();
             void GetProd()
             {                
                 try
-                {
-                    
+                {                    
                     da.SelectCommand = new SqlCommand(SelectCommand, con);
                     da.Fill(dt);
-               
-
                 }
 
-                catch (Exception e)
+                catch (Exception e)   
                 {
-
                     _update?.Invoke(e.Message);
-
                 }
             }
             return dt;
@@ -78,7 +55,6 @@ namespace ShopManager
         {
             try
             {
-
                 da.Update(dt);
             }
             catch (Exception e)
